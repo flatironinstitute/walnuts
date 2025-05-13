1a2c7158925ad3e18b768b03567d5bf277fea02e
Bob Carpenter <bcarpenter@flatironinstitute.org>
2025-05-13 16:25:10 -0400

    added WALNUTS (heuristic)

--- examples/test.cpp
+++ examples/test.cpp
@@ -1,4 +1,5 @@
 #include <walnuts/nuts.hpp>
+#include <walnuts/walnuts.hpp>
 #include <chrono>
 #include <cmath>
 #include <iostream>
@@ -24,28 +25,21 @@ void standard_normal_logp_grad(const Eigen::Matrix<T, Eigen::Dynamic, 1> &x,
   ++count;
 }
 
-int main() {
-  int init_seed = 333456;
-  int seed = 763545;
-  int D = 200;
-  int N = 5000;
-  S step_size = 0.025;
-  int max_depth = 10;
-  VectorS inv_mass = VectorS::Ones(D);
+template <bool W, typename G>
+void test_nuts(const VectorS& theta_init, G& generator, int D, int N, S step_size, S max_depth,
+	       S max_error, const VectorS& inv_mass) {
   MatrixS draws(D, N);
-  std::cout << "D = " << D << ";  N = " << N << ";  step_size = " << step_size
-            << ";  max_depth = " << max_depth << std::endl;
-
-  std::mt19937 generator(init_seed);
-  std::normal_distribution<S> std_normal(0.0, 1.0);
-  VectorS theta_init(D);
-  for (int i = 0; i < D; ++i) {
-    theta_init(i) = std_normal(generator);
-  }
+  std::cout << std::endl << "D = " << D << ";  N = " << N << ";  step_size = " << step_size
+            << ";  max_depth = " << max_depth << ";  WALNUTS = " << (W ? "true" : "false") << std::endl;
 
   auto global_start = std::chrono::high_resolution_clock::now();
-  nuts::nuts(generator, standard_normal_logp_grad<S>, inv_mass, step_size,
-             max_depth, theta_init, draws);
+  if constexpr (W) {
+    walnuts::walnuts(generator, standard_normal_logp_grad<S>, inv_mass, step_size,
+		     max_depth, max_error, theta_init, draws);
+  } else {
+    nuts::nuts(generator, standard_normal_logp_grad<S>, inv_mass, step_size,
+	       max_depth, theta_init, draws);
+  }
   auto global_end = std::chrono::high_resolution_clock::now();
   auto global_total_time =
       std::chrono::duration<double>(global_end - global_start).count();
@@ -59,7 +53,7 @@ int main() {
             << std::endl;
   std::cout << std::endl;
 
-  for (int d = 0; d < std::min(D, 10); ++d) {
+  for (int d = 0; d < std::min(D, 5); ++d) {
     auto mean = draws.row(d).mean();
     auto var = (draws.row(d).array() - mean).square().sum() / (N - 1);
     auto stddev = std::sqrt(var);
@@ -67,7 +61,28 @@ int main() {
               << "\n";
   }
   if (D > 10) {
-    std::cout << "... elided " << (D - 10) << " dimensions ..." << std::endl;
+    std::cout << "... elided " << (D - 5) << " dimensions ..." << std::endl;
   }
+}
+
+int main() {
+  int seed = 763545;
+  int D = 200;
+  int N = 5000;
+  S step_size = 0.1;
+  int max_depth = 10;
+  S max_error = 0.05;
+  VectorS inv_mass = VectorS::Ones(D);
+
+  std::mt19937 generator(seed);
+  std::normal_distribution<S> std_normal(0.0, 1.0);
+  VectorS theta_init(D);
+  for (int i = 0; i < D; ++i) {
+    theta_init(i) = std_normal(generator);
+  }
+
+  test_nuts<false>(theta_init, generator, D, N, step_size, max_depth, max_error, inv_mass);
+  test_nuts<true>(theta_init, generator, D, N, step_size, max_depth, max_error, inv_mass);
+    
   return 0;
 }

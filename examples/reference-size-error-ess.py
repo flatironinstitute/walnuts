cc50e42285ad6235a90068496ba8169362b6878d
Bob Carpenter <bcarpenter@flatironinstitute.org>
2025-08-11 17:22:15 -0400

    fix math in ess plot

--- examples/reference-size-error-ess.py
+++ examples/reference-size-error-ess.py
@@ -27,34 +27,14 @@ def compute_stats(ref_size, test_sizes, n_sims, n_runs):
         })
     return stats
 
-n_sims=5000
-test_sizes = [1000]
-# np.logspace(np.log10(100), np.log10(10_000), 20, dtype=int)
-for n_runs in [256, 512, 1028]:
+n_sims=16
+test_sizes = [1024]
+np.logspace(np.log10(100), np.log10(10_000), 20, dtype=int)
+for n_runs in [8, 16, 32, 64, 128, 256, 512, 1028]:
     print(f"\n***** {n_runs=}")
-    for ref_size in [10_000, 100_000]:
-        ref_sizes = [ref_size] # [ref_size, np.inf]
-        
-        all_stats = []
-        for ref_size in ref_sizes:
-            all_stats.extend(compute_stats(ref_size, test_sizes, n_sims, n_runs))
+    ref_size = 100_000
+    all_stats = []
+    all_stats.extend(compute_stats(ref_size, test_sizes, n_sims, n_runs))
+    df = pd.DataFrame(all_stats)
+    print(df)
 
-        df = pd.DataFrame(all_stats)
-        print(df)
-
-        
-#         plot = (
-#             ggplot(df, aes(x='test_size'))
-#             + geom_line(aes(y='mean', color='ref_size', fill='ref_size'), size=1)
-#             + geom_ribbon(aes(ymin='lower', ymax='upper', fill='ref_size', group='ref_size'), alpha=0.2)
-#             + scale_x_log10(breaks=[1e0, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7])
-#     + scale_y_log10()
-#     + labs(
-#         x='test sample size',
-#         y='estimated ESS',
-#         title=f"Number of test runs: {n_runs}",
-#         color='reference Size',
-#         fill='reference Size'
-#     )
-# )
-# plot.show()

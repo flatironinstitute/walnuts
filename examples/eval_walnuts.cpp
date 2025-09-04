649c423853c199221ffea1b907b60937ef8ba79e
Bob Carpenter <bcarpenter@flatironinstitute.org>
2025-09-04 10:44:47 -0400

    no shared warmup; Dirichlet

--- examples/eval_walnuts.cpp
+++ examples/eval_walnuts.cpp
@@ -79,17 +79,17 @@ static void test_adaptive_walnuts(const DynamicStanModel& model,
   nuts::MassAdaptConfig mass_cfg(mass_init, init_count, mass_iteration_offset,
                                  additive_smoothing);
   
-  double step_size_init = 1.0;
+  double step_size_init = 0.1;
   double accept_rate_target = 0.8;  // min 2.0 / 3.0
-  double step_iteration_offset = 5.0;
-  double learning_rate = 1.5;
+  double step_iteration_offset = 2.0;
+  double learning_rate = 2.0;
   double decay_rate = 0.05;
   nuts::StepAdaptConfig step_cfg(step_size_init, accept_rate_target,
                                  step_iteration_offset, learning_rate,
                                  decay_rate);
 
-  double max_error = 1000;  // 1000: NUTS; 1.0: 37% accept; 0.5: 62%; 0.2: 82%
-  Integer max_nuts_depth = 10;
+  double max_error = 0.5;  // 1000: NUTS; 1.0: 37% accept; 0.5: 62%; 0.2: 82%
+  Integer max_nuts_depth = 8;
   Integer max_step_depth = 6;
   nuts::WalnutsConfig walnuts_cfg(max_error, max_nuts_depth, max_step_depth);
 
--- examples/models/dirichlet/dirichlet.stan
+++ examples/models/dirichlet/dirichlet.stan
@@ -2,11 +2,11 @@ data {
   int<lower=0> N;
 }
 transformed data {
-	    //  vector<lower=0>[N] alpha = exp(linspaced_vector(N, log(1), log(1)));
+  vector<lower=0>[N] alpha = exp(linspaced_vector(N, log(1.0 / N), log(N)));
 }
 parameters {
   simplex[N] theta;	   
 }
 model {
-  // theta ~ dirichlet(alpha);
+  theta ~ dirichlet(alpha);
 }

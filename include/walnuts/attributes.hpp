976b4056aba33410245424a475b447b873dc13e6
Steve Bronder <sbronder@flatironinstitute.org>
2026-01-21 13:14:04 -0500

    update attributes.hpp for msvc

--- include/walnuts/attributes.hpp
+++ include/walnuts/attributes.hpp
@@ -82,4 +82,37 @@
 #define unlikely(x) __builtin_expect((x), 0)
 #endif
 
+#else // ifdef __has_attribute
+
+#ifndef WALNUTS_NO_INLINE_
+#define WALNUTS_NO_INLINE_
 #endif
+
+#ifndef WALNUTS_ALWAYS_INLINE_
+#define WALNUTS_ALWAYS_INLINE_ inline
+#endif
+
+#ifndef WALNUTS_HOT_
+#define WALNUTS_HOT_
+#endif
+
+#ifndef WALNUTS_COLD_
+#define WALNUTS_COLD_
+#endif
+
+#ifndef WALNUTS_FLATTEN_
+#define WALNUTS_FLATTEN_
+#endif
+
+#ifndef WALNUTS_PURE_
+#define WALNUTS_PURE_
+#endif
+
+#ifndef likely
+#define likely(x) __builtin_expect((x), 1)
+#endif
+
+#ifndef unlikely
+#define unlikely(x) __builtin_expect((x), 0)
+#endif
+#endif // ifdef __has_attribute

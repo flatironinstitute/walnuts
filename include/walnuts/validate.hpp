e8108991d9ac1746988168c0d9795d160fea4c06
Bob Carpenter <bcarpenter@flatironinstitute.org>
2026-04-27 17:16:40 -0400

    inline validation funs

--- include/walnuts/validate.hpp
+++ include/walnuts/validate.hpp
@@ -8,21 +8,21 @@
 namespace nuts {
 
   template <typename T1, typename T2>
-  void validate_same_size(const T1& x1, const T2& x2,
-			  const std::string& fun,
-			  const std::string& arg1,
-			  const std::string& arg2) {
+  inline void validate_same_size(const T1& x1, const T2& x2,
+                                 const std::string& fun,
+                                 const std::string& arg1,
+                                 const std::string& arg2) {
     if (x1.size() != x2.size()) {
       throw std::invalid_argument(fun + ": " + arg1 + " and " + arg2 + " must be same size");
     }
   }
 
   template <typename F, typename T>
-  void validate_elements(const F& f,
-			 T x,
-			 std::string fun,
-			 std::string arg,
-			 std::string err) {
+  inline void validate_elements(const F& f,
+                                T x,
+                                const std::string& fun,
+                                const std::string& arg,
+                                const std::string& err) {
     if (!f(x)) {
       std::string msg = fun + " argument " + arg + " " + err;
       throw std::invalid_argument(msg);
@@ -30,22 +30,22 @@ namespace nuts {
   }
 
   template <typename F, typename T, int M, int N>
-  void validate_elements(const F& f,
-			 const Eigen::Matrix<T, M, N>& x,
-			 const std::string& fun,
-			 const std::string& arg,
-			 const std::string& err) {
+  inline void validate_elements(const F& f,
+                                const Eigen::Matrix<T, M, N>& x,
+                                const std::string& fun,
+                                const std::string& arg,
+                                const std::string& err) {
     for (int64_t n = 0; n < x.size(); ++n) {
       validate_elements(f, x(n), fun, arg, err);
     }
   }
   
-  template <typename T, typename F>
-  void validate_elements(const F& test,
-			 const std::vector<T>& xs,
-			 const std::string& fun,
-			 const std::string& arg,
-			 const std::string& err) {
+  template <typename F, typename T>
+  inline void validate_elements(const F& test,
+                                const std::vector<T>& xs,
+                                const std::string& fun,
+                                const std::string& arg,
+                                const std::string& err) {
     for (const T& x : xs) {
       validate_elements(test, x, fun, arg, err);
     }
@@ -53,96 +53,96 @@ namespace nuts {
   
 
   template <typename T>
-  void validate_positive(T x, std::string fun, std::string arg) {
+  inline void validate_positive(T x,
+                                const std::string& fun,
+                                const std::string& arg) {
     auto is_pos = [](auto u) { return u > 0; };
     validate_elements(is_pos, x, fun, arg, "must be positive");
   }
 
 
   template <std::floating_point T>
-  void validate_probability(T p, const std::string& var) {
+  inline void validate_probability(T p, const std::string& var) {
     if (!(p > 0 && p < 1))
       throw std::invalid_argument(var + " must be a proportion in (0, 1)");
   }
   
   template <typename T>
-  void validate_size(const std::vector<T>& x, uint64_t size,
-		     const std::string& var, const std::string& target) {
+  inline void validate_size(const std::vector<T>& x, uint64_t size,
+                            const std::string& var, const std::string& target) {
     if (x.size() != size)
       throw std::invalid_argument(var + " size must match " + target);
   }
 
   template <typename T, int R, int C>
-  void validate_size(const Eigen::Matrix<T, R, C>& x, uint64_t size,
-		     const std::string& var, const std::string& target) {
+  inline void validate_size(const Eigen::Matrix<T, R, C>& x, uint64_t size,
+                            const std::string& var, const std::string& target) {
     if (x.size() != static_cast<int64_t>(size))
       throw std::invalid_argument(var + " size must match " + target);
   }
 
   template <typename T>
-  void validate_gt0(T x, const std::string& var) {
+  inline void validate_gt0(T x, const std::string& var) {
     if (!(x > 0))
       throw std::invalid_argument(var + " must be > 0");
   }
 
   template <std::floating_point T>
-  void validate_finite_gt1(T x, const std::string& var) {
+  inline void validate_finite_gt1(T x, const std::string& var) {
     if (!(std::isfinite(x) && x > 1))
       throw std::invalid_argument(var + " must be finite and > 1");
   }
 
   template <std::integral T>
-  void validate_finite_positive(T x, const std::string& var) {
+  inline void validate_finite_positive(T x, const std::string& var) {
     if (!(x > 0))
-        throw std::invalid_argument(var + " must be > 0");
+      throw std::invalid_argument(var + " must be > 0");
   }
 
   template <std::floating_point T>
-  void validate_finite_positive(T x, const std::string& var) {
+  inline void validate_finite_positive(T x, const std::string& var) {
     if (!(std::isfinite(x) && x > 0))
-        throw std::invalid_argument(var + " must be finite and > 0");
+      throw std::invalid_argument(var + " must be finite and > 0");
   }
   
   template <typename T, int R, int C>
-  void validate_finite_positive(const Eigen::Matrix<T, R, C>& xs, const std::string& var) {
+  inline void validate_finite_positive(const Eigen::Matrix<T, R, C>& xs, const std::string& var) {
     std::string var_entries = var + " entries";
-    for (int64_t i = 0; i < xs.size(); ++i)
+    for (Eigen::Index i = 0; i < xs.size(); ++i)
       validate_finite_positive(xs(i), var_entries);
   }
 
   template <typename T>
-  void validate_finite_positive(const std::vector<T>& xs, const std::string& var) {
+  inline void validate_finite_positive(const std::vector<T>& xs, const std::string& var) {
     std::string var_entries = var + " entries";
     for (const auto& x : xs)
       validate_finite_positive(x, var_entries);
   }
 
   template <std::floating_point T>
-  void validate_finite(T x, const std::string& var) {
+  inline void validate_finite(T x, const std::string& var) {
     if (!std::isfinite(x))
-        throw std::invalid_argument(var + " must be finite and > 0");
+      throw std::invalid_argument(var + " must be finite");
   }
   
   template <typename T, int R, int C>
-  void validate_finite(const Eigen::Matrix<T, R, C>& xs, const std::string& var) {
+  inline void validate_finite(const Eigen::Matrix<T, R, C>& xs, const std::string& var) {
     std::string var_entries = var + " entries";
     for (int64_t i = 0; i < xs.size(); ++i)
       validate_finite(xs(i), var_entries);
   }
 
   template <typename T>
-  void validate_finite(const std::vector<T>& xs, const std::string& var) {
+  inline void validate_finite(const std::vector<T>& xs, const std::string& var) {
     std::string var_entries = var + " entries";
     for (const auto& x : xs)
       validate_finite(x, var_entries);
   }
-
-
   
   template <class Stream>
-  void validate_open(const Stream& s, const std::string& name) {
+  inline void validate_open(const Stream& s, const std::string& name) {
     if (!s.is_open()) {
-      throw std::invalid_argument("could not open stream from : " + name);
+      throw std::invalid_argument("could not open stream from: " + name);
     }
   }
 }
